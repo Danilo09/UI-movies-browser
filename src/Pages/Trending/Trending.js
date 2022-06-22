12283025cb2244b7643cce41f05fab71eaf3e12a
Danilo <danilo.nascimento2@hotmail.com>
2022-06-22 14:40:52 +0200

    Fetch API and config env

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -1,7 +1,33 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
 const Trending = () => {
+
+    const [content, setContent] = useState([]);
+
+    const fetchTrending = async () => {
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+        );
+        console.log(data)
+        setContent(data.results);
+    };
+
+    useEffect(() => {
+        fetchTrending()
+
+    }, [])
+
     return (
         <div>
             <span className='pageTitle'>Trending</span>
+            <div className='trending'>
+                {
+                    content && content.map((c) => (
+                        console.log(c)
+                    ))
+                }
+            </div>
         </div>
     )
 }
